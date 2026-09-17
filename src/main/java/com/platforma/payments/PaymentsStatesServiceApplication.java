c06e7004acd0efc7ee928803ec2510ca61eeb251
Gomes <54913387+luana-gomes@users.noreply.github.com>
2026-09-17 16:50:36 -0300

    feat: estrutura payments-states-service

---
+++ src/main/java/com/platforma/payments/PaymentsStatesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.platforma.payments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentsStatesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentsStatesServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/platforma/payments/PaymentsStatesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platforma.payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsStatesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
